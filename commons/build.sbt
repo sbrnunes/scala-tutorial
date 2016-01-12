c56cb5b257b55b63d764ebc66543c1b34c8a7e56
Sergio Nunes <sbrnunes@gmail.com>
2016-01-12 23:20:04 +0000

    Moving model file to finatra server module

--- commons/build.sbt
+++
@@ -1,6 +0,0 @@
-
-lazy val versions = new {
-  val finatra = "2.1.2"
-}
-
-libraryDependencies +=  "com.twitter.finatra" %% "finatra-http" % versions.finatra
--- commons/src/main/scala/com/scala/tutorial/model/HelloRequest.scala
+++
@@ -1,7 +0,0 @@
-package com.scala.tutorial.model
-
-import com.twitter.finatra.request.QueryParam
-
-case class HelloRequest(
-  @QueryParam first: String,
-  @QueryParam last: String)
